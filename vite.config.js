acced3b55d190c37eca8f75ed44e4221c2c6cd90
LSkyler13 <luangskyler13@gmail.com>
2025-11-05 16:23:19 -0500

    trying to make new domain work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc' // Change this line
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lotus-site/',
+  base: '/',   // or just remove the base line
   plugins: [react()],
 })
